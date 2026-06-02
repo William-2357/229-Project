4baf1b8062521f2a7f4beba5c1e9c220ab159da2
williammtan <william.h.tan27@gmail.com>
2026-06-02 03:35:11 +0000

    add k adaptive anchored cld

--- modal_runner.py
+++ modal_runner.py
@@ -108,6 +108,7 @@ SUPERVISED = {
     "foundation_sft_finetune", "foundation_sft_lora", "foundation_sft_ea_lora",
     "foundation_sft_cld", "foundation_sft_ea_cld",
     "foundation_sft_anchored_cld", "foundation_sft_ea_anchored_cld",
+    "foundation_sft_kadaptive_anchored_cld",
 }
 
 
@@ -176,6 +177,9 @@ def run_job(
     from adaptation.foundation_sft_anchored_cld import (
         FoundationSFTAnchoredCLDAdapter, FoundationSFTAnchoredEACLDAdapter
     )
+    from adaptation.foundation_sft_kadaptive_anchored_cld import (
+        FoundationSFTKAdaptiveAnchoredCLDAdapter
+    )
     from evaluation.protocols import loso_evaluation, k_minute_sweep
     from evaluation.results import save_result
 
@@ -207,6 +211,7 @@ def run_job(
         "foundation_sft_ea_cld": FoundationSourceFineTuneEACLDAdapter,
         "foundation_sft_anchored_cld": FoundationSFTAnchoredCLDAdapter,
         "foundation_sft_ea_anchored_cld": FoundationSFTAnchoredEACLDAdapter,
+        "foundation_sft_kadaptive_anchored_cld": FoundationSFTKAdaptiveAnchoredCLDAdapter,
     }
 
     torch.manual_seed(seed)
